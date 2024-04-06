4068762239720b8d03f731300682b71b92a1873c
Rodrigo Alejandro Ramos Lozano <85802575+rodrigocoris@users.noreply.github.com>
2024-04-06 12:53:41 -0600

    New Title

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,6 +4,6 @@
     <div class="container py-5 text-center">
         <h1>Bienvenido al CRUD de LARAVEL</h1>
         <br>
-        <a href="{{ route('client.index') }}" class="btn btn-primary">Clientes</a>
+        <a href="{{ route('client.index') }}" class="btn btn-primary">Listado de Clientes</a>
     </div>
 @endsection
